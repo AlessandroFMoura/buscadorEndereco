f435a9df6c537f12feccbd49aff55ac6c8df3725
alessandrofmoura <alessandronahal@gmail.com>
2025-06-13 16:48:08 -0300

    Adicionando um looping

--- src/GeradorDeArquivo.java
+++ src/GeradorDeArquivo.java
@@ -5,26 +5,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class GeradorDeArquivo {
-
-//    private BuscaCep endereco = new BuscaCep();
-
-
     public void gerar(Endereco cep) throws IOException {
+
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
         FileWriter json = new FileWriter(cep.cep()+".json");
         json.write(gson.toJson(cep));
         json.close();
     }
-
-//    public String gerar(String cep) throws IOException {
-//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//
-//        Endereco arq = this.endereco.buscarCep(cep);
-//        FileWriter json = new FileWriter("resultadoBusca.json");
-//        json.write(gson.toJson(arq));
-//        json.close();
-//        return arq.toString();
-//    }
-
 }
--- src/Principal.java
+++ src/Principal.java
@@ -4,21 +4,33 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Informe seu CEP: ");
-        String cep = scan.nextLine().replaceAll("[-. //s]", "");;
-
-
+        String cep = "";
 
         try {
-            BuscaCep cep1 = new BuscaCep();
-            System.out.println(cep1.buscarCep(cep));
-            GeradorDeArquivo novoEndereco = new GeradorDeArquivo();
-            novoEndereco.gerar(cep1.buscarCep(cep));
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-            System.out.println("Finalizando a Aplicação.");
+            while (!cep.equalsIgnoreCase("sair")) {
+
+                System.out.print("Informe seu CEP: ");
+                cep = scan.nextLine().replaceAll("[-.\\s]", "");
+
+                if (cep.equalsIgnoreCase("sair")) {
+                    break;
+                }
+
+                try {
+                    BuscaCep cep1 = new BuscaCep();
+                    System.out.println(cep1.buscarCep(cep));
+                    GeradorDeArquivo novoEndereco = new GeradorDeArquivo();
+                    novoEndereco.gerar(cep1.buscarCep(cep));
+
+                } catch (RuntimeException e) {
+                    System.out.println(e.getMessage());
+                    System.out.println("Tente novamente!");
+
+                }
+            }
+        }finally {
+                scan.close();
+                System.out.println("Finalizando a Aplicação.");
         }
-
-
     }
 }
